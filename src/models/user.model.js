aa61ce23f2a066349fbb57aaffa27c2968cd04b8
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-03-12 16:50:33 +0530

    Added bcrypt"Implemented password hashing using bcrypt in user schema pre-save hook."

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     email: {
@@ -12,6 +13,13 @@ const userSchema = new Schema({
     },
 }, {
     timestamps: { createdAt: "createdAT", updatedAt: "updatedAt" }
+})
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
 })
 
 const userModel = mongoose.model('User', userSchema);
