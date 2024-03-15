9c482da0e7dd6e7139edeac8d26c3d5f94790760
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-03-15 17:54:24 +0530

    Add try catch block to handle error, if jwt expired user redirect to login page

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,15 +2,23 @@ import User from '../models/user.model.js';
 import jwt from 'jsonwebtoken';
 
 export const verifyJwt = async (req, res, next) => {
-    const token = req.cookies.accessToken || (req.headers.authorization && req.headers.authorization.split("Bearer ")[1]);
-    if(!token){
-        return res.redirect('/auth/login')
+    try {
+        const token = req.cookies.accessToken || (req.headers.authorization && req.headers.authorization.split("Bearer ")[1]);
+        if(!token){
+            return res.redirect('/auth/login')
+        }
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findById(decodedToken?.id);
+        res.locals.user = user
+        req.user = user
+        next()
+    } catch (error) {
+        if (error.message === 'jwt expired') {
+            res.clearCookie('accessToken');
+            res.redirect('/auth/login')
+            // return res.redirect('/auth/login')
+        }
     }
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    const user = await User.findById(decodedToken?.id);
-    res.locals.user = user
-    req.user = user
-    next()
 }
 
 export const ensureLoggedout = async (req, res, next) => {
